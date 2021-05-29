97242b528cabd1024d7d8142a890cc39fb6ee11e
Laxman Bista <lax.bista@gmail.com>
2021-05-28 20:21:22 -0600

    api fix

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,7 @@ const { json } = require('body-parser');
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
-    apiKey: 'fcf4eeef63340f6be050e8b6eaa9542'
+    apiKey: '5fcf4eeef63340f6be050e8b6eaa9542'
   });
 const handleApiCall = (req,res) =>{
 app.models
